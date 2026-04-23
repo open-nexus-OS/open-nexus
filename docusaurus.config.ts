ac44a022e5d2ac6b8e5d38d0992d9e89b33b900e
jenningschaefer <jenningschaefer@googlemail.com>
2026-04-23 09:20:03 +0200

    add Google Tag Manager integration
    Injects GTM script in <head> and noscript fallback at the start of <body> via Docusaurus headTags and bodyTagsBefore config options.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -28,6 +28,26 @@ const config: Config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
+  headTags: [
+    {
+      tagName: 'script',
+      attributes: {},
+      innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5BQJP3N2');`,
+    },
+  ],
+
+  bodyTagsBefore: [
+    {
+      tagName: 'noscript',
+      attributes: {},
+      innerHTML: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5BQJP3N2" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+    },
+  ],
+
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
